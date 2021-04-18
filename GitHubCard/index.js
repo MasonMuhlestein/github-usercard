a441d93674257d93c5d63e9fe43d38266807107d
Mason Muhlestein <shadowmas12@gmail.com>
2021-04-17 23:23:48 -0600

    all done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,20 @@
 /*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
+STEP 1: using axios, send a GET request to the following URL
+(replacing the placeholder with your Github name):
+https://api.github.com/users/<your name>
 */
+import axios from 'axios'
+axios.get(`https://api.github.com/users/MasonMuhlestein`)
+.then(res => {
+  console.log(res)
+  const ref = res.data
+  const cards = document.querySelector('.cards')
+  cards.appendChild(makeCard(ref))
+}) 
+.catch(err => {
+  console.log(err)
+})
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,12 +40,34 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
 
+
+const followersArray = [
+    "tetondan",
+    "dustinmyers",
+    "justsml",
+    "luishrd",
+    "bigknell",
+];
+
+followersArray.forEach(followers => {
+  axios.get(`https://api.github.com/users/${followers}`)
+    .then(res => {
+      const ref = res.data
+      const cards = document.querySelector('.cards')
+      cards.appendChild(makeCard(ref))
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+
+
+
+})
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -50,6 +84,61 @@ const followersArray = [];
     </div>
 */
 
+function makeCard(arg) {
+  axios.get(arg)
+  console.log(arg)
+//============================================//
+//creating tags for the elements//
+  const userCard = document.createElement('div');
+  const image = document.createElement('img');
+  const infoCard = document.createElement('div');
+  const Name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const Location = document.createElement('p');
+  const Profile = document.createElement('p');
+  const gitLink = document.createElement('a');
+  const Followers = document.createElement('p');
+  const Following = document.createElement('p');
+  const bioInfo = document.createElement('p');
+//===========================================//
+//Data//
+  image.src = arg.avatar_url;
+  Name.textContent = arg.name;
+  userName.textContent = arg.login;
+  Location.textContent = `Location: ${arg.Location}`;
+  Profile.textContent = `profile:`
+  gitLink.href = arg.html_url;
+  gitLink.textContent = arg.html_url
+  Followers.textContent = `Followers: ${arg.followers}`
+  Following.textContent = `Following: ${arg.following}`
+  bioInfo.textContent = `Bio: ${arg.bio}`
+
+//===============================================//
+//adding classes//
+  userCard.classList.add('card')
+  infoCard.classList.add('card-info');
+  Name.classList.add('name');
+  userName.classList.add('username');
+//=====================================//
+//creating a heirarchy//
+
+  userCard.appendChild(image);
+  userCard.appendChild(infoCard);
+
+  infoCard.appendChild(Name);
+  infoCard.appendChild(userName);
+  infoCard.appendChild(Location);
+  infoCard.appendChild(Profile);
+  Profile.appendChild(gitLink);
+  infoCard.appendChild(Followers);
+  infoCard.appendChild(Following);
+  infoCard.appendChild(bioInfo);
+//======================================//
+// return usercard becuase its the parent and will return everything else //
+
+return userCard;
+}
+ makeCard(res);
 /*
   List of LS Instructors Github username's:
     tetondan
